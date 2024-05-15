6b91970e7932edaba9fb6eaad798b826ceb4b338
miguelit0 <meduardofuentesc@gmail.com>
2024-05-15 11:34:04 -0500

    initial commit

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>App</h1>
+  <h1>home</h1>
 </template>
---
+++ src/pages/SplashScreen/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <img src="../assets/cashflow-icon.svg" alt="Logo" />
+    <h1>Cash<span>flow</span></h1>
+  </div>
+</template>
+
+<style scoped></style>
